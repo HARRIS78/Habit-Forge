daea1097f3f6bcc225cb839dd9eb8a1ac92f7632
HARISH <83852605+HARRIS78@users.noreply.github.com>
2024-06-26 10:10:08 +0530

    Update WelcomeMessage.jsx

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -2,7 +2,8 @@ import styles from "./WelcomeMessage.module.css"
 const WelcomeMessage=({todoItems})=>
 {
   return <>{todoItems.length === 0 && <h4 className={styles.heading}>
-  You have not any challenge pending, you might want to add <br />
+  You have not any challenge pending, you might want to add for example : NO SUGAR FOR 7 DAYS, NO MASTURABATION FOR 50 DAYS etc.
+    <br />
    Or <br />
    you have completed all your challenges so congratulations !</h4>}</>;
 };
